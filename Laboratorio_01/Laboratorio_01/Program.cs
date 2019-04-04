69187d618dfd2b972f4c921e2259aa9033d07779
Bernardo Duarte <31990193+bernardodsf@users.noreply.github.com>
2019-04-04 17:21:20 -0300

    Add files via upload

---
+++ Laboratorio_01/Laboratorio_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vetor2D vet1 = new Vetor2D(10.0, 5.0);
+            Vetor2D vet2 = new Vetor2D(4.0, -2.0);
+
+            Console.WriteLine("Produto escalar entre os 2 vetores: " + vet1.produtoEscalar(vet2));
+
+            Console.WriteLine("Modulo do vetor 1: " + vet1.moduloVetor());
+            Console.WriteLine("Modulo do vetor 2: " + vet2.moduloVetor());
+
+            Console.WriteLine("Angulo entre os vetores: " + vet1.anguloVetor(vet2) + " graus");
+
+            Vetor2D vetprojecao = new Vetor2D();
+            vetprojecao = vet1.projecaoVetor(vet2);
+            Console.WriteLine("Projecao do vetor 1 no vetor 2: (" + vetprojecao.getX() + ", " + vetprojecao.getY() + ")");
+            vetprojecao = vet2.projecaoVetor(vet1);
+            Console.WriteLine("Projecao do vetor 2 no vetor 1: (" + vetprojecao.getX() + ", " + vetprojecao.getY() + ")");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Laboratorio_01/Laboratorio_01/Vetor2D.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_01
+{
+    class Vetor2D
+    {
+        //atributos
+        private double X;
+        private double Y;
+
+
+        //construtor default
+        public Vetor2D()
+        {
+            X = 0.0;
+            Y = 0.0;
+        }
+
+
+        //construtor variável
+        public Vetor2D(double _X, double _Y) 
+        {
+            X = _X;
+            Y = _Y;
+        }
+
+
+        //getters e setters
+        public double getX()
+        {
+            return X;
+        }
+
+        public void setX(double _X)
+        {
+            X = _X;
+        }
+
+        public double getY()
+        {
+            return Y;
+        }
+
+        public void setY(double _Y)
+        {
+            Y = _Y;
+        }
+
+
+        //produto escalar entre os 2 vetores
+        public double produtoEscalar(Vetor2D vetor)
+        {
+            return (X * vetor.getX() + Y * vetor.getY());
+        }
+
+
+        //módulo do vetor
+        public double moduloVetor()
+        {
+            return Math.Sqrt(Math.Pow(X, 2.0) + Math.Pow(Y, 2.0));
+        }
+
+
+        //angulo entre dois vetores
+        public double anguloVetor(Vetor2D vetor)
+        {
+            double ang;
+            ang = (produtoEscalar(vetor) / (moduloVetor() * vetor.moduloVetor()));
+            return (Math.Acos(ang) * 180 / Math.PI);
+        }
+
+
+        //projeção vetor A em B
+        public Vetor2D projecaoVetor(Vetor2D vetor)
+        {
+            double aux;
+            Vetor2D vetorProjecao = new Vetor2D();
+            aux = (produtoEscalar(vetor) / produtoEscalar(this));
+            vetorProjecao.setX(getX() * aux);
+            vetorProjecao.setY(getY() * aux);
+            return vetorProjecao;
+        }
+
+    }
+}
